6264acd6d4057f65c7792b13b7da9419b11ed7cb
kuldeep <kuldeepkumar2416@gmail.com>
2026-03-15 22:59:45 +0530

    this is new branch

---
+++ app/component/Newpost.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Newpost = () => {
+    return (
+        <View>
+            <Text>Newpost</Text>
+            <Text>this is new branch post </Text>
+            <Text>Newpost</Text>
+            <Text>Newpost</Text>
+        </View>
+    )
+}
+
+export default Newpost
